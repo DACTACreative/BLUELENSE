96e2e14505d9b41ef96f468fb66a55c1404dfa30
Eunice Mac Mini M4 <eunicemacmini@Mac.gateway>
2025-07-27 15:16:25 +1000

    Update blue-lense project with new components and modifications

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import SimpleTest from "./pages/SimpleTest";
 import LandingPage from "./pages/LandingPage";
 import NotFound from "./pages/NotFound";
 import Scenario1 from "./pages/Scenario1";
